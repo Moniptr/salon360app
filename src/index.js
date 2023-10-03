469942e104755f430777f0978fec8cd2bc25f5ac
Monika Patidar <monikapatidar0907@gmail.com>
2023-10-03 15:39:05 +0530

    "main index"

--- src/index.js
+++ src/index.js
@@ -16,12 +16,8 @@ import SideBar from "./components/sidebar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-      {/* <Header />
-      <Aboutus />
-      <Monthly/> */}
-      {/* <Tryresponsive/> */}
-      {/* <Knowmore /> */}
-      {/* <Business/> s */}
+      
+      
       <SideBar />
   </React.StrictMode>
 );
